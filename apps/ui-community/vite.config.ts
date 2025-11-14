18ebe38ba3f3a41f016c9d5679d071f88c9bffcb
Nick Noce <nnoce12@gmail.com>
2025-11-14 14:34:08 -0500

    ci(azure-pipelines): reverted test changes on pipeline

--- apps/ui-community/vite.config.ts
+++ apps/ui-community/vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, type PluginOption } from 'vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() as PluginOption, visualizer() as PluginOption],
