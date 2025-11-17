ef7c90ea261440f464ecf90de16f9e7be50f4847
Nick Noce <nnoce12@gmail.com>
2025-11-17 09:56:09 -0500

    chore(ui-community): include rollup-plugin-visualizer only in development; ci(azure-pipelines): pass NODE_ENV into buildEnvSettings

--- apps/ui-community/vite.config.ts
+++ apps/ui-community/vite.config.ts
@@ -2,9 +2,15 @@ import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, type PluginOption } from 'vite'
 
+const { NODE_ENV } = process.env;
+const isDev = NODE_ENV === 'development';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() as PluginOption, visualizer() as PluginOption],
+  plugins: [
+    react() as PluginOption,
+    ...(isDev ? [visualizer() as PluginOption] : []),
+  ],
   server: {
     port: 3000,
   },
