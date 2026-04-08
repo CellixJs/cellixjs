f9035fa06a846580edf241e8eead350d98b878d8
Rohit Kumar <rkumar@ecfmg.org>
2026-04-08 20:30:30 +0530

    Fix: Member details

--- apps/ui-community/src/components/layouts/admin/components/members-detail.container.tsx
+++ apps/ui-community/src/components/layouts/admin/components/members-detail.container.tsx
@@ -1,8 +1,13 @@
 import type React from 'react';
 import { useQuery } from '@apollo/client';
-import { ComponentQueryLoader } from '../../../shared/ui/molecules/component-query-loader/component-query-loader.tsx';
+import { ComponentQueryLoader } from '@cellix/ui-core';
 import { MembersDetail } from './members-detail.tsx';
-import { AdminMembersDetailContainerMembersByCommunityDocument, type AdminMembersDetailContainerMembersByCommunityQuery, type AdminMembersDetailContainerMembersByCommunityQueryVariables } from '../../../../generated.tsx';
+import {
+	AdminMembersDetailContainerMembersByCommunityDocument,
+	type AdminMembersDetailContainerMemberFieldsFragment,
+	type AdminMembersDetailContainerMembersByCommunityQuery,
+	type AdminMembersDetailContainerMembersByCommunityQueryVariables,
+} from '../../../../generated.tsx';
 
 export interface MembersDetailContainerProps {
 	data: {
@@ -21,18 +26,11 @@ export const MembersDetailContainer: React.FC<MembersDetailContainerProps> = (pr
 	// Find the specific member by ID
 	const selectedMember = data?.membersByCommunityId?.find((member) => member.id === props.data.id);
 
-	const membersDetailProps = {
-		data: {
-			member: selectedMember,
-		},
-	};
-
 	return (
 		<ComponentQueryLoader
 			loading={loading}
-			hasData={!!selectedMember}
-			hasDataComponent={<MembersDetail {...membersDetailProps} />}
-			query={AdminMembersDetailContainerMembersByCommunityDocument}
+			hasData={selectedMember}
+			hasDataComponent={<MembersDetail data={{ member: selectedMember as AdminMembersDetailContainerMemberFieldsFragment }} />}
 			error={error}
 		/>
 	);
