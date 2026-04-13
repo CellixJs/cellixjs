e28839634a19cd5fc7731f4e301b8fb8059d0c9c
Rohit Kumar <rkumar@ecfmg.org>
2026-04-13 23:37:58 +0530

    fix: knip issues

--- packages/ocom/ui-community-route-root/.storybook/main.ts
+++ packages/ocom/ui-community-route-root/.storybook/main.ts
@@ -1,12 +1,21 @@
-module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
-  },
+import { dirname, join } from 'node:path';
+import type { StorybookConfig } from '@storybook/react-vite';
+
+function getAbsolutePath(value: string) {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		getAbsolutePath('@chromatic-com/storybook'),
+		getAbsolutePath('@storybook/addon-docs'),
+		getAbsolutePath('@storybook/addon-a11y'),
+		getAbsolutePath('@storybook/addon-vitest'),
+	],
+	framework: {
+		name: getAbsolutePath('@storybook/react-vite'),
+		options: {},
+	},
 };
+export default config;
