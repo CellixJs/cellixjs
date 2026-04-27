337388cd3a129aff902b896c1c0902051b000a30
Rohit Kumar <rkumar@ecfmg.org>
2026-04-28 00:25:20 +0530

    Update line in codegen.yml

--- packages/ocom/graphql/src/schema/types/end-user-role.resolvers.ts
+++ packages/ocom/graphql/src/schema/types/end-user-role.resolvers.ts
@@ -8,21 +8,17 @@ const endUserRole: Resolvers = {
 			if (!context.applicationServices.verifiedUser?.verifiedJwt) {
 				throw new Error('Unauthorized');
 			}
-			const roles = await context.applicationServices.Community.Role.EndUserRole.queryByCommunityId({
+			return await context.applicationServices.Community.Role.EndUserRole.queryByCommunityId({
 				communityId: args.communityId,
 			});
-			// biome-ignore lint/suspicious/noExplicitAny: GraphQL codegen type mismatch with domain types
-			return roles as any;
 		},
 		endUserRole: async (_parent, args: { id: string }, context: GraphContext, _info: GraphQLResolveInfo) => {
 			if (!context.applicationServices.verifiedUser?.verifiedJwt) {
 				throw new Error('Unauthorized');
 			}
-			const role = await context.applicationServices.Community.Role.EndUserRole.queryById({
+			return await context.applicationServices.Community.Role.EndUserRole.queryById({
 				id: args.id,
 			});
-			// biome-ignore lint/suspicious/noExplicitAny: GraphQL codegen type mismatch with domain types
-			return role as any;
 		},
 	},
 };
