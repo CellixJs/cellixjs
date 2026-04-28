8edc386cd899a1f6f033f5a261518f12bdb6c280
Jason Morais <jasonmorais02@gmail.com>
2026-04-28 15:14:17 -0400

    small adjustment

--- packages/cellix/archunit-tests/src/cellix.test.ts
+++ packages/cellix/archunit-tests/src/cellix.test.ts
@@ -1,5 +1,8 @@
 import { describeDependencyRulesTests } from './test-suites/dependency-rules.js';
 
 describeDependencyRulesTests({
-	packagesGlob: '../**/src/**',
+	packagesGlob: '../{cellix}/**',
+	uiCoreFolder: '../cellix/ui-core',
+	uiComponentsFolder: '../ocom/ui-components',
+	appUiFolder: '../../apps/ui-community',
 });
