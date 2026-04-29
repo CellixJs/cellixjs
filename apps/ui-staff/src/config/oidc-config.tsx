0a7817fe584c95856b3f777a773c3452b3ffa2ad
Trang Nguyen <TNguyen@intealth.org>
2026-04-29 10:14:31 -0400

    update OIDC configuration and add mock data for staff authentication

--- apps/ui-staff/src/config/oidc-config.tsx
+++ apps/ui-staff/src/config/oidc-config.tsx
@@ -1,13 +1,31 @@
-export const oidcConfig = {
-	// minimal placeholder config for local development
-	authority: import.meta.env.VITE_AAD_B2C_ACCOUNT_AUTHORITY ?? 'https://mock-auth.staff.localhost',
-	client_id: import.meta.env.VITE_AAD_B2C_ACCOUNT_CLIENTID ?? 'mock-client',
-	redirect_uri: import.meta.env.VITE_AAD_B2C_REDIRECT_URI ?? 'https://staff.localhost/auth-redirect',
+type OIDCConfig = {
+	authority: string;
+	client_id: string;
+	redirect_uri: string;
+	code_verifier: boolean;
+	noonce: boolean;
+	response_type: string;
+	scope: string;
+	onSigninCallback: () => void;
+};
+
+export const oidcConfig: OIDCConfig = {
+	authority:
+		// biome-ignore lint:useLiteralKeys
+		import.meta.env['VITE_AAD_B2C_STAFF_AUTHORITY'] ?? 'https://mock-auth.ownercommunity.localhost:1355/staff',
+	// biome-ignore lint:useLiteralKeys
+	client_id: import.meta.env['VITE_AAD_B2C_STAFF_CLIENTID'] ?? 'mock-client',
+
+	redirect_uri:
+		// biome-ignore lint:useLiteralKeys
+		import.meta.env['VITE_AAD_B2C_STAFF_REDIRECT_URI'] ?? 'https://staff.ownercommunity.localhost:1355/auth-redirect',
 	code_verifier: true,
 	noonce: true,
 	response_type: 'code',
-	scope: import.meta.env.VITE_AAD_B2C_ACCOUNT_SCOPES,
+	// biome-ignore lint:useLiteralKeys
+	scope: import.meta.env['VITE_AAD_B2C_STAFF_SCOPES'],
 	onSigninCallback: (): void => {
+		console.log('onSigninCallback');
 		globalThis.history.replaceState({}, document.title, globalThis.location.pathname);
 		const redirectToPath = globalThis.sessionStorage.getItem('redirectTo');
 		if (redirectToPath) {
