23854af7804f6392958c3ea0ea1473d167595494
Rohit Kumar <rkumar@ecfmg.org>
2026-05-29 19:59:39 +0530

    Refactor test cases

--- packages/ocom/service-token-validation/src/index.test.ts
+++ packages/ocom/service-token-validation/src/index.test.ts
@@ -97,7 +97,7 @@ test.for(feature, ({ Scenario, BeforeEachScenario }) => {
 		And('it should pass the default refresh interval to VerifiedTokenService', () => {
 			expect(VerifiedTokenService).toHaveBeenCalledWith(expect.any(Map), 1000 * 60 * 5);
 		});
-		And('it should initialize VerifiedTokenService', () => {
+		And('it should store the VerifiedTokenService instance', () => {
 			expect(VerifiedTokenService).toHaveBeenCalledOnce();
 		});
 	});
